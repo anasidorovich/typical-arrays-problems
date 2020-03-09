be097e66a86f848837c055fd20b186b56cda8773
sidarova <anasidorovich@gmail.com>
2020-03-09 16:39:22 +0300

    feat: implement max, min and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return Array.isArray(array) && array.length ? Math.min.apply(null, array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return Array.isArray(array) && array.length ? Math.max.apply(null, array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return Array.isArray(array) && array.length ? array.reduce((a,b) => a + b, 0) / array.length : 0;
 }
